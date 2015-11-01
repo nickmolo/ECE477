f2ad127d8f53fe12b4634a23d8976e72262aac71
Nick Molo <nick.molo@gmail.com>
2015-11-01 17:17:23 -0500

    adding working hal library to start development for

---
+++ Software/SmartWatch/Inc/mxconstants.h
@@ -0,0 +1,107 @@
+/**
+  ******************************************************************************
+  * File Name          : mxconstants.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2015 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define OSC32_IN_Pin GPIO_PIN_14
+#define OSC32_IN_GPIO_Port GPIOC
+#define OSC_IN_Pin GPIO_PIN_0
+#define OSC_IN_GPIO_Port GPIOH
+#define BT_SCK_Pin GPIO_PIN_5
+#define BT_SCK_GPIO_Port GPIOA
+#define BT_MISO_Pin GPIO_PIN_6
+#define BT_MISO_GPIO_Port GPIOA
+#define BT_MOSI_Pin GPIO_PIN_7
+#define BT_MOSI_GPIO_Port GPIOA
+#define BT_INT_Pin GPIO_PIN_4
+#define BT_INT_GPIO_Port GPIOC
+#define BTN_DN_Pin GPIO_PIN_0
+#define BTN_DN_GPIO_Port GPIOB
+#define BTN_SEL_Pin GPIO_PIN_1
+#define BTN_SEL_GPIO_Port GPIOB
+#define BTN_UP_Pin GPIO_PIN_2
+#define BTN_UP_GPIO_Port GPIOB
+#define SCL_Pin GPIO_PIN_13
+#define SCL_GPIO_Port GPIOB
+#define SDA_Pin GPIO_PIN_14
+#define SDA_GPIO_Port GPIOB
+#define G_INT_Pin GPIO_PIN_8
+#define G_INT_GPIO_Port GPIOC
+#define IMU_INT_Pin GPIO_PIN_9
+#define IMU_INT_GPIO_Port GPIOC
+#define IMU_NRESET_Pin GPIO_PIN_8
+#define IMU_NRESET_GPIO_Port GPIOA
+#define BTN_BACK_Pin GPIO_PIN_10
+#define BTN_BACK_GPIO_Port GPIOA
+#define USB_D__Pin GPIO_PIN_11
+#define USB_D__GPIO_Port GPIOA
+#define USB_D_A12_Pin GPIO_PIN_12
+#define USB_D_A12_GPIO_Port GPIOA
+#define SWDIO_Pin GPIO_PIN_13
+#define SWDIO_GPIO_Port GPIOA
+#define SWCLK_Pin GPIO_PIN_14
+#define SWCLK_GPIO_Port GPIOA
+#define _ALRT_Pin GPIO_PIN_15
+#define _ALRT_GPIO_Port GPIOA
+#define OLED_SCK_Pin GPIO_PIN_10
+#define OLED_SCK_GPIO_Port GPIOC
+#define OLED_MOSI_Pin GPIO_PIN_12
+#define OLED_MOSI_GPIO_Port GPIOC
+#define RED_Pin GPIO_PIN_3
+#define RED_GPIO_Port GPIOB
+#define YELLOW_Pin GPIO_PIN_4
+#define YELLOW_GPIO_Port GPIOB
+#define GREEN_Pin GPIO_PIN_5
+#define GREEN_GPIO_Port GPIOB
+#define OLED_RES_Pin GPIO_PIN_8
+#define OLED_RES_GPIO_Port GPIOB
+#define OLED_DC_Pin GPIO_PIN_9
+#define OLED_DC_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
